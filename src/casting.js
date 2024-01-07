c07f804a0d660f75559516cfc7a8d1fc8b0dbce4
Lucas Moreira <lucazmoreira@gmail.com>
2024-01-07 17:48:41 -0300

    commit inicial

---
+++ src/casting.js
@@ -0,0 +1,8 @@
+"use strict";
+var casting;
+(function (casting) {
+  let idade = 28;
+  let nome = "Lucas";
+  idade.toFixed();
+  nome.length;
+})(casting || (casting = {}));
---
+++ src/casting.ts
@@ -0,0 +1,6 @@
+namespace casting {
+  let idade: any = 28;
+  let nome: any = "Lucas";
+  (idade as number).toFixed();
+  (nome as string).length;
+}
